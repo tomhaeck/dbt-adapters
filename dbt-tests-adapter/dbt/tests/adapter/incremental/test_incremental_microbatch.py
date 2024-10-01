344a54a4739c2b1374f7c58b91570f358f2b1303
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-10-01 12:20:04 -0500

    Remove unused imports from test_incremental_microbatch

--- dbt-tests-adapter/dbt/tests/adapter/incremental/test_incremental_microbatch.py
+++ dbt-tests-adapter/dbt/tests/adapter/incremental/test_incremental_microbatch.py
@@ -1,6 +1,4 @@
-import os
 from pprint import pformat
-from unittest import mock
 
 import pytest
 
@@ -69,8 +67,8 @@ class BaseMicrobatch:
             "flags": {
                 "require_builtin_microbatch_strategy": True,
             }
-    }
-    
+        }
+
     def test_run_with_event_time(self, project, insert_two_rows_sql):
         # initial run -- backfills all data
         with patch_microbatch_end_time("2020-01-03 13:57:00"):
